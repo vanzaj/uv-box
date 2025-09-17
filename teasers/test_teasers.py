a72ada2512f0fe1c24de10f456b52f0692e8bd43
Ivan <vanzaj@gmail.com>
2025-09-17 15:51:58 +0700

    py teasers

--- teasers/test_teasers.py
+++ teasers/test_teasers.py
@@ -1,5 +1,9 @@
+import pytest
+
+
 class Player:
     count = 0
+
     def __init__(self, name):
         self.name = name
         self.count += 1
@@ -9,3 +13,15 @@ def test_player_init():
     p = Player("Bob")
     assert p.count == 1
     assert Player.count == 0
+
+
+def test_unicode():
+    assert len("Krakoẃ") == 6  # with digraph from vim
+    assert len("Kraków") == 7
+    assert len("Kraków") == 6
+
+
+def test_almost_equal():
+    x = 1.1
+    # assert x * x == 1.21
+    assert pytest.approx(x * x) == 1.21
